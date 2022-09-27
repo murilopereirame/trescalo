65aade7aff9999721027b188948a25a23cd5c122
Murilo Pereira <contato@murilopereira.dev.br>
2022-09-27 00:17:51 -0300

    chore: Base project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>Hello World</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
 
@@ -20,8 +16,15 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+}
+
+body,
+html {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
 }
 </style>
